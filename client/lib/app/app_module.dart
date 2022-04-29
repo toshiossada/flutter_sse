cd62e6063949ab2c46fd2108b260128f6fe248fc
Toshi Ossada <toshiossada@gmail.com>
2022-04-29 08:41:24 -0300

    .

--- client/lib/app/app_module.dart
+++ client/lib/app/app_module.dart
@@ -1,15 +1,13 @@
 import 'package:client/app/modules/home/home_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-
 import '../commons/adapter/sse/implementations/eventsource_sse_adapter_impl.dart';
-import '../commons/adapter/sse/implementations/mock_sse_adapter_impl.dart';
 import '../commons/adapter/sse/sse_adapter.dart';
 
 class AppModule extends Module {
   @override
   List<Bind> get binds => [
-        Bind.factory<SseAdapter>((i) => MockSseAdapterImpl()),
+        Bind.factory<SseAdapter>((i) => EventsourceSseAdapterImpl()),
       ];
 
   @override
--- client/lib/commons/adapter/sse/implementations/mock/mock.dart
+++ client/lib/commons/adapter/sse/implementations/mock/mock.dart
@@ -1,30 +1,30 @@
 final response0 = {
-  "name": 0,
+  "id": 0,
   "message": "event 0",
   "finished": false,
 };
 final response1 = {
-  "name": 1,
+  "id": 1,
   "message": "event 1",
   "finished": false,
 };
 final response2 = {
-  "name": 2,
+  "id": 2,
   "message": "event 2",
   "finished": false,
 };
 final response3 = {
-  "name": 3,
+  "id": 3,
   "message": "event 3",
   "finished": false,
 };
 final response4 = {
-  "name": 4,
+  "id": 4,
   "message": "event 4",
   "finished": false,
 };
 final response5 = {
-  "name": 5,
+  "id": 5,
   "message": "event 5",
   "finished": true,
 };
